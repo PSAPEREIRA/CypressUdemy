a23c601f009c9246235dc4d202d58dda5ee99643
Pedro Sa Pereira <pedro.duarte.sapereira@gmail.com>
2022-01-09 20:32:54 +0000

    Fixtures added
    Signed-off-by: Pedro Sa Pereira <pedro.duarte.sapereira@gmail.com>

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -1,22 +1,35 @@
 /// <reference types="Cypress" />
 
 describe("Test Contact Us form via Automation Test Store", () => {
-    it("Sould be able to submit a sucessfull submission via contact us form", () => {
+
+    beforeEach(function () {
+        cy.fixture("userDetails").as("user")
         cy.visit("https://www.automationteststore.com/");
 
-           cy.get("a[href$='contact']").click().then(function(itemHeaderText){
-            cy.log("Selected the folowing item: " + itemHeaderText.text()) 
-           });
-           //cy.xpath("//a[contains(@href, 'contact')]").click();
-           cy.get('#ContactUsFrm_first_name').type("John");
-           cy.get('#ContactUsFrm_email').type("john.doe@gmail.com");
-           cy.get('#ContactUsFrm_email').should('have.attr','name','email');
+    })
+    it("Sould be able to submit a sucessfull submission via contact us form", () => {
 
-           cy.get('#ContactUsFrm_enquiry').type("Will I learn cypress");
-           //cy.get('.col-md-6 > .btn').click(); 
-           cy.get("button[title='Submit']").click();
-           cy.get('.mb40 > :nth-child(3)').should('have.text','Your enquiry has been successfully sent to the store owner!')
+        //cy.visit("https://www.automationteststore.com/");
 
+        cy.get("a[href$='contact']").click().then(function (itemHeaderText) {
+            cy.log("Selected the folowing item: " + itemHeaderText.text())
         });
-   
+
+        //cy.xpath("//a[contains(@href, 'contact')]").click();
+        cy.get("@user").then((user) => {
+            cy.get('#ContactUsFrm_first_name').type(user.first_name);
+            cy.get('#ContactUsFrm_email').type(user.email);
+        })
+        /*Deprecated by the use of fixtures above
+        cy.get('#ContactUsFrm_first_name').type("John");
+        cy.get('#ContactUsFrm_email').type("john.doe@gmail.com");*/
+        cy.get('#ContactUsFrm_email').should('have.attr', 'name', 'email');
+
+        cy.get('#ContactUsFrm_enquiry').type("Will I learn cypress");
+        //cy.get('.col-md-6 > .btn').click(); 
+        cy.get("button[title='Submit']").click();
+        cy.get('.mb40 > :nth-child(3)').should('have.text', 'Your enquiry has been successfully sent to the store owner!')
+
+    });
+
 })
--- cypress/integration/webdriver-uni/contac-us.js
+++ cypress/integration/webdriver-uni/contac-us.js
@@ -1,30 +1,44 @@
 /// <reference types="Cypress" />
 
 describe("Test Contact Us form via WebdriverUni", () => {
-    it("Sould be able to submit a sucessfull via contact us form", () => {
-        
-    cy.visit("http://www.webdriveruniversity.com")
-    cy.get('#contact-us').invoke('removeAttr','target').click({force:true})
 
-    //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
-    cy.document().should('have.property','charset').and('eq','UTF-8');
-    cy.title().should('include','WebDriver | Contact Us');
-    cy.url().should('include','contactus');
-    
-    
-    cy.get('[name="first_name"]').type("jhon")
-    cy.get('[name="last_name"]').type("doe")
-    cy.get('[name="email"]').type("john.doe@gmail.com")
-    cy.get('textarea.feedback-input').type("whatever john has to say")
-    cy.get('[type="submit"]').click();
-    cy.get('h1').should('have.text', 'Thank You for your Message!');
-});
-    it("Sould mot be able to submit sucessfully via contact us form as all fields are required", () =>{
-        
-        //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+    beforeEach(function () {
+        cy.fixture('example').then(function (data) {
+            //this.data = data; //local run of the fixture file if it doesnt work use line below
+            globalThis.data = data;
+        })
+
         cy.visit("http://www.webdriveruniversity.com")
-        cy.get('#contact-us').invoke('removeAttr','target').click({force:true})
-        
+        cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true })
+
+    })
+    it("Sould be able to submit a sucessfull via contact us form", () => {
+
+        /* Deprecated by the use off hook before, left for example  
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true })*/ 
+
+        //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
+        cy.title().should('include', 'WebDriver | Contact Us');
+        cy.url().should('include', 'contactus');
+
+
+        cy.get('[name="first_name"]').type(data.first_name) // using fixtures
+        cy.get('[name="last_name"]').type(data.last_name)
+        cy.get('[name="email"]').type(data.email)
+        cy.get('textarea.feedback-input').type("whatever joe (previous was john that was hardcoded) has to say")
+        cy.get('[type="submit"]').click();
+        cy.get('h1').should('have.text', 'Thank You for your Message!');
+    });
+
+    it("Sould mot be able to submit sucessfully via contact us form as all fields are required", () => {
+
+        /* Same as above example
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true })*/
+
         cy.get('[name="first_name"]').type("jhon")
         cy.get('[name="last_name"]').type("doe")
         cy.get('textarea.feedback-input').type("whatever john has to say")
